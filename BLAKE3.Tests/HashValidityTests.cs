dcddbdf89480c0cca5c79f6bb917cfecf489c33a
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-02-09 21:57:10 -0600

    Improved tests to actually be fully discovered instead of running out of memory

--- BLAKE3.Tests/HashValidityTests.cs
+++ BLAKE3.Tests/HashValidityTests.cs
@@ -11,27 +11,30 @@ namespace BLAKE3.Tests
 {
     public class HashValidityTests
     {
-        private static string TestDataPath = 
+        private static readonly string TestDataPath = 
             Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TestData");
         private static string DataFile(int i)
             => Path.Combine(TestDataPath, $"test_data_{i}");
+        private static byte[] ReadData(int i)
+            => File.ReadAllBytes(DataFile(i));
         private static string HashFile(int i)
             => Path.Combine(TestDataPath, $"test_data_{i}.b3");
+        private static byte[] ReadHash(int i)
+            => File.ReadAllBytes(HashFile(i));
 
         public static IEnumerable<object?[]> GetHashData(int count)
             => Enumerable.Range(1, count)
-                         .Select(i => new object[] 
-                         { 
-                             File.ReadAllBytes(DataFile(i)), 
-                             File.ReadAllBytes(HashFile(i)) 
-                         });
+                         .Select(i => new object[] { i });
 
         private readonly BLAKE3 hasher = new BLAKE3(); 
 
         [Theory]
         [MemberData(nameof(GetHashData), 8)]
-        public void CheckHashing(byte[] input, byte[] expectedHash)
+        public void CheckHashing(int testRun)
         {
+            var input = ReadData(testRun);
+            var expectedHash = ReadHash(testRun);
+
             var hashed = hasher.ComputeHash(input);
             Assert.True(Utilities.BytesEqual(hashed, expectedHash));
         }
